06ada907e058b3a1dd114549cde541d706e5bc7f
송찬호 <chanho@hhi.co.kr>
2018-07-11 15:03:16 +0900

    renamed code

--- src/main/scala/com/hhi/sap/analysis/ANL_THD_MRPL_WEEK.scala
+++ src/main/scala/com/hhi/sap/analysis/ANL_THD_MRPL_WEEK.scala
@@ -4,12 +4,12 @@ import com.hhi.sap.analysis.functions.MRPLTableUtils
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.slf4j.LoggerFactory
 
-class ANL_THD_MRPL_WEEKLY(sql: SQLContext) {
+class ANL_THD_MRPL_WEEK(sql: SQLContext) {
   private val logger = LoggerFactory.getLogger(this.getClass)
 
-  def run(zpdct6023: DataFrame): DataFrame = genTable(zpdct6023)
+  def run(zpdct6123: DataFrame): DataFrame = genTable(zpdct6123)
 
-  private def genTable(zpdct6023: DataFrame): DataFrame = {
+  private def genTable(zpdct6123: DataFrame): DataFrame = {
     import sql.sparkSession.implicits._
 
     var PGMID = "Spark2.3.0.cloudera2"
@@ -20,7 +20,7 @@ class ANL_THD_MRPL_WEEKLY(sql: SQLContext) {
       CNAM = "[DEBUGMODE]A504863"
     }
 
-    val mrplRDD = MRPLTableUtils.getMRPLRDD(zpdct6023)
+    val mrplRDD = MRPLTableUtils.getMRPLRDD(zpdct6123)
     val underRDD = MRPLTableUtils.getWeekTable(mrplRDD.filter(_.week <= -5), -5)
     val upperRDD = MRPLTableUtils.getWeekTable(mrplRDD.filter(_.week >= 20), 20)
 
