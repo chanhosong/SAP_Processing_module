31ad2653bd5d89677457efef69a334ba0ad271da
송찬호 <chanho@hhi.co.kr>
2018-07-26 11:11:20 +0900

    changed value name

--- src/main/scala/com/hhi/sap/analysis/ANL_THD_MRPL_WEEK.scala
+++ src/main/scala/com/hhi/sap/analysis/ANL_THD_MRPL_WEEK.scala
@@ -14,11 +14,11 @@ class ANL_THD_MRPL_WEEK(sql: SQLContext) {
     import sql.sparkSession.implicits._
 
     val mrplRDD = MRPLTableUtils.getMRPLRDD(zpdct6123)
-    val underRDD = MRPLTableUtils.getWeekTable(mrplRDD.filter(_.week <= -5), -5)
-    val upperRDD = MRPLTableUtils.getWeekTable(mrplRDD.filter(_.week >= 20), 20)
+    val underDF = MRPLTableUtils.getWeekTable(mrplRDD.filter(_.week <= -5), -5)
+    val upperDF = MRPLTableUtils.getWeekTable(mrplRDD.filter(_.week >= 20), 20)
 
     TransformUtils
-      .makeUnion(mrplRDD.filter(-4 until 19 contains _.week).toDF(), underRDD, upperRDD)
+      .makeUnion(mrplRDD.filter(-4 until 19 contains _.week).toDF(), underDF, upperDF)
       .transform(TransformUtils.pivotTableByCount)
       .transform(TransformUtils.mappingTable)
       .transform(TransformUtils.addSERNO)
--- src/main/scala/com/hhi/sap/analysis/ANL_THD_WEIGHT_WEEK.scala
+++ src/main/scala/com/hhi/sap/analysis/ANL_THD_WEIGHT_WEEK.scala
@@ -43,11 +43,11 @@ class ANL_THD_WEIGHT_WEEK(sql: SQLContext) {
     )
 
     val weightRDD = WeightTableUtils.getWeightRDD(joinedRDD, mara)
-    val underRDD = WeightTableUtils.getWeightTable(weightRDD.filter(_.week <= -5), -5)
-    val upperRDD = WeightTableUtils.getWeightTable(weightRDD.filter(_.week >= 20), 20)
+    val underDF = WeightTableUtils.getWeightTable(weightRDD.filter(_.week <= -5), -5)
+    val upperDF = WeightTableUtils.getWeightTable(weightRDD.filter(_.week >= 20), 20)
 
     TransformUtils
-      .makeUnion(weightRDD.filter(-4 until 19 contains _.week).toDF(), underRDD, upperRDD)
+      .makeUnion(weightRDD.filter(-4 until 19 contains _.week).toDF(), underDF, upperDF)
       .transform(TransformUtils.pivotTableByBrgew)
       .transform(TransformUtils.mappingTable)
       .transform(TransformUtils.addSERNO)
